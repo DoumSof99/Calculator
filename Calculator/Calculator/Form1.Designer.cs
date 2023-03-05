e0e3f5baa78ee541aa330d2bf356a66599e50e41
DoumSof99 <sofiadmn99@gmail.com>
2023-03-05 23:13:26 +0200

    First validations and some TODOs

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctrl1 = new System.Windows.Forms.Button();
+            this.ctrl2 = new System.Windows.Forms.Button();
+            this.ctrl3 = new System.Windows.Forms.Button();
+            this.ctrl4 = new System.Windows.Forms.Button();
+            this.ctrl5 = new System.Windows.Forms.Button();
+            this.ctrl6 = new System.Windows.Forms.Button();
+            this.ctrl7 = new System.Windows.Forms.Button();
+            this.ctrl8 = new System.Windows.Forms.Button();
+            this.ctrl9 = new System.Windows.Forms.Button();
+            this.ctrlDot = new System.Windows.Forms.Button();
+            this.ctrl0 = new System.Windows.Forms.Button();
+            this.ctrlEquals = new System.Windows.Forms.Button();
+            this.ctrlAdd = new System.Windows.Forms.Button();
+            this.ctrlMinus = new System.Windows.Forms.Button();
+            this.ctrlMultiply = new System.Windows.Forms.Button();
+            this.ctrlDivide = new System.Windows.Forms.Button();
+            this.ctrlCE = new System.Windows.Forms.Button();
+            this.ctrlC = new System.Windows.Forms.Button();
+            this.ctrlText = new System.Windows.Forms.TextBox();
+            this.ctrlOperation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(399, 98);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "           ";
+            // 
+            // ctrl1
+            // 
+            this.ctrl1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl1.Location = new System.Drawing.Point(26, 380);
+            this.ctrl1.Name = "ctrl1";
+            this.ctrl1.Size = new System.Drawing.Size(99, 68);
+            this.ctrl1.TabIndex = 2;
+            this.ctrl1.Text = "1";
+            this.ctrl1.UseVisualStyleBackColor = true;
+            this.ctrl1.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl2
+            // 
+            this.ctrl2.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl2.Location = new System.Drawing.Point(131, 380);
+            this.ctrl2.Name = "ctrl2";
+            this.ctrl2.Size = new System.Drawing.Size(94, 68);
+            this.ctrl2.TabIndex = 3;
+            this.ctrl2.Text = "2";
+            this.ctrl2.UseVisualStyleBackColor = true;
+            this.ctrl2.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl3
+            // 
+            this.ctrl3.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl3.Location = new System.Drawing.Point(231, 380);
+            this.ctrl3.Name = "ctrl3";
+            this.ctrl3.Size = new System.Drawing.Size(89, 68);
+            this.ctrl3.TabIndex = 4;
+            this.ctrl3.Text = "3";
+            this.ctrl3.UseVisualStyleBackColor = true;
+            this.ctrl3.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl4
+            // 
+            this.ctrl4.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl4.Location = new System.Drawing.Point(26, 306);
+            this.ctrl4.Name = "ctrl4";
+            this.ctrl4.Size = new System.Drawing.Size(99, 68);
+            this.ctrl4.TabIndex = 5;
+            this.ctrl4.Text = "4";
+            this.ctrl4.UseVisualStyleBackColor = true;
+            this.ctrl4.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl5
+            // 
+            this.ctrl5.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl5.Location = new System.Drawing.Point(131, 306);
+            this.ctrl5.Name = "ctrl5";
+            this.ctrl5.Size = new System.Drawing.Size(94, 68);
+            this.ctrl5.TabIndex = 6;
+            this.ctrl5.Text = "5";
+            this.ctrl5.UseVisualStyleBackColor = true;
+            this.ctrl5.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl6
+            // 
+            this.ctrl6.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl6.Location = new System.Drawing.Point(231, 306);
+            this.ctrl6.Name = "ctrl6";
+            this.ctrl6.Size = new System.Drawing.Size(89, 68);
+            this.ctrl6.TabIndex = 7;
+            this.ctrl6.Text = "6";
+            this.ctrl6.UseVisualStyleBackColor = true;
+            this.ctrl6.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl7
+            // 
+            this.ctrl7.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl7.Location = new System.Drawing.Point(26, 232);
+            this.ctrl7.Name = "ctrl7";
+            this.ctrl7.Size = new System.Drawing.Size(99, 68);
+            this.ctrl7.TabIndex = 8;
+            this.ctrl7.Text = "7";
+            this.ctrl7.UseVisualStyleBackColor = true;
+            this.ctrl7.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl8
+            // 
+            this.ctrl8.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl8.Location = new System.Drawing.Point(131, 232);
+            this.ctrl8.Name = "ctrl8";
+            this.ctrl8.Size = new System.Drawing.Size(94, 68);
+            this.ctrl8.TabIndex = 9;
+            this.ctrl8.Text = "8";
+            this.ctrl8.UseVisualStyleBackColor = true;
+            this.ctrl8.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl9
+            // 
+            this.ctrl9.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl9.Location = new System.Drawing.Point(231, 232);
+            this.ctrl9.Name = "ctrl9";
+            this.ctrl9.Size = new System.Drawing.Size(89, 68);
+            this.ctrl9.TabIndex = 10;
+            this.ctrl9.Text = "9";
+            this.ctrl9.UseVisualStyleBackColor = true;
+            this.ctrl9.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrlDot
+            // 
+            this.ctrlDot.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlDot.Location = new System.Drawing.Point(26, 454);
+            this.ctrlDot.Name = "ctrlDot";
+            this.ctrlDot.Size = new System.Drawing.Size(99, 68);
+            this.ctrlDot.TabIndex = 11;
+            this.ctrlDot.Text = ".";
+            this.ctrlDot.UseVisualStyleBackColor = true;
+            this.ctrlDot.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrl0
+            // 
+            this.ctrl0.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrl0.Location = new System.Drawing.Point(131, 454);
+            this.ctrl0.Name = "ctrl0";
+            this.ctrl0.Size = new System.Drawing.Size(189, 68);
+            this.ctrl0.TabIndex = 12;
+            this.ctrl0.Text = "0";
+            this.ctrl0.UseVisualStyleBackColor = true;
+            this.ctrl0.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrlEquals
+            // 
+            this.ctrlEquals.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlEquals.Location = new System.Drawing.Point(231, 158);
+            this.ctrlEquals.Name = "ctrlEquals";
+            this.ctrlEquals.Size = new System.Drawing.Size(194, 68);
+            this.ctrlEquals.TabIndex = 13;
+            this.ctrlEquals.Text = "=";
+            this.ctrlEquals.UseVisualStyleBackColor = true;
+            this.ctrlEquals.Click += new System.EventHandler(this.EqualButton_click);
+            // 
+            // ctrlAdd
+            // 
+            this.ctrlAdd.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlAdd.Location = new System.Drawing.Point(326, 232);
+            this.ctrlAdd.Name = "ctrlAdd";
+            this.ctrlAdd.Size = new System.Drawing.Size(99, 68);
+            this.ctrlAdd.TabIndex = 14;
+            this.ctrlAdd.Text = "+";
+            this.ctrlAdd.UseVisualStyleBackColor = true;
+            this.ctrlAdd.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrlMinus
+            // 
+            this.ctrlMinus.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlMinus.Location = new System.Drawing.Point(326, 306);
+            this.ctrlMinus.Name = "ctrlMinus";
+            this.ctrlMinus.Size = new System.Drawing.Size(99, 68);
+            this.ctrlMinus.TabIndex = 15;
+            this.ctrlMinus.Text = "-";
+            this.ctrlMinus.UseVisualStyleBackColor = true;
+            this.ctrlMinus.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrlMultiply
+            // 
+            this.ctrlMultiply.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlMultiply.Location = new System.Drawing.Point(326, 380);
+            this.ctrlMultiply.Name = "ctrlMultiply";
+            this.ctrlMultiply.Size = new System.Drawing.Size(99, 68);
+            this.ctrlMultiply.TabIndex = 16;
+            this.ctrlMultiply.Text = "*";
+            this.ctrlMultiply.UseVisualStyleBackColor = true;
+            this.ctrlMultiply.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrlDivide
+            // 
+            this.ctrlDivide.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlDivide.Location = new System.Drawing.Point(326, 454);
+            this.ctrlDivide.Name = "ctrlDivide";
+            this.ctrlDivide.Size = new System.Drawing.Size(99, 68);
+            this.ctrlDivide.TabIndex = 17;
+            this.ctrlDivide.Text = "/";
+            this.ctrlDivide.UseVisualStyleBackColor = true;
+            this.ctrlDivide.Click += new System.EventHandler(this.Button_click);
+            // 
+            // ctrlCE
+            // 
+            this.ctrlCE.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlCE.Location = new System.Drawing.Point(131, 158);
+            this.ctrlCE.Name = "ctrlCE";
+            this.ctrlCE.Size = new System.Drawing.Size(94, 68);
+            this.ctrlCE.TabIndex = 18;
+            this.ctrlCE.Text = "CE";
+            this.ctrlCE.UseVisualStyleBackColor = true;
+            this.ctrlCE.Click += new System.EventHandler(this.CEButton_click);
+            // 
+            // ctrlC
+            // 
+            this.ctrlC.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlC.Location = new System.Drawing.Point(26, 158);
+            this.ctrlC.Name = "ctrlC";
+            this.ctrlC.Size = new System.Drawing.Size(99, 68);
+            this.ctrlC.TabIndex = 19;
+            this.ctrlC.Text = "C";
+            this.ctrlC.UseVisualStyleBackColor = true;
+            this.ctrlC.Click += new System.EventHandler(this.CButton_click);
+            // 
+            // ctrlText
+            // 
+            this.ctrlText.BackColor = System.Drawing.Color.Silver;
+            this.ctrlText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ctrlText.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold);
+            this.ctrlText.ForeColor = System.Drawing.Color.Black;
+            this.ctrlText.Location = new System.Drawing.Point(41, 77);
+            this.ctrlText.Name = "ctrlText";
+            this.ctrlText.Size = new System.Drawing.Size(370, 43);
+            this.ctrlText.TabIndex = 20;
+            this.ctrlText.Text = "0";
+            this.ctrlText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ctrlOperation
+            // 
+            this.ctrlOperation.AutoSize = true;
+            this.ctrlOperation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ctrlOperation.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.ctrlOperation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ctrlOperation.Location = new System.Drawing.Point(45, 45);
+            this.ctrlOperation.Name = "ctrlOperation";
+            this.ctrlOperation.Size = new System.Drawing.Size(0, 21);
+            this.ctrlOperation.TabIndex = 21;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(447, 534);
+            this.Controls.Add(this.ctrlOperation);
+            this.Controls.Add(this.ctrlText);
+            this.Controls.Add(this.ctrlC);
+            this.Controls.Add(this.ctrlCE);
+            this.Controls.Add(this.ctrlDivide);
+            this.Controls.Add(this.ctrlMultiply);
+            this.Controls.Add(this.ctrlMinus);
+            this.Controls.Add(this.ctrlAdd);
+            this.Controls.Add(this.ctrlEquals);
+            this.Controls.Add(this.ctrl0);
+            this.Controls.Add(this.ctrlDot);
+            this.Controls.Add(this.ctrl9);
+            this.Controls.Add(this.ctrl8);
+            this.Controls.Add(this.ctrl7);
+            this.Controls.Add(this.ctrl6);
+            this.Controls.Add(this.ctrl5);
+            this.Controls.Add(this.ctrl4);
+            this.Controls.Add(this.ctrl3);
+            this.Controls.Add(this.ctrl2);
+            this.Controls.Add(this.ctrl1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ctrl1;
+        private System.Windows.Forms.Button ctrl2;
+        private System.Windows.Forms.Button ctrl3;
+        private System.Windows.Forms.Button ctrl4;
+        private System.Windows.Forms.Button ctrl5;
+        private System.Windows.Forms.Button ctrl6;
+        private System.Windows.Forms.Button ctrl7;
+        private System.Windows.Forms.Button ctrl8;
+        private System.Windows.Forms.Button ctrl9;
+        private System.Windows.Forms.Button ctrlDot;
+        private System.Windows.Forms.Button ctrl0;
+        private System.Windows.Forms.Button ctrlEquals;
+        private System.Windows.Forms.Button ctrlAdd;
+        private System.Windows.Forms.Button ctrlMinus;
+        private System.Windows.Forms.Button ctrlMultiply;
+        private System.Windows.Forms.Button ctrlDivide;
+        private System.Windows.Forms.Button ctrlCE;
+        private System.Windows.Forms.Button ctrlC;
+        private System.Windows.Forms.TextBox ctrlText;
+        private System.Windows.Forms.Label ctrlOperation;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        private bool shouldAddToListAndText = false;
+        private bool isOperatorPerforemed = false;
+        private List<string> lsSymbols = new List<string>() { "+", "-", "*", "/" };
+        private List<string> lsValues = new List<string> ();
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            if (ctrlText.Text == "0" || isOperatorPerforemed) ctrlText.Clear();
+
+            if (lsSymbols.Contains(button.Text))
+            {
+                ctrlText.Text = button.Text;
+                isOperatorPerforemed = true;
+                shouldAddToListAndText = false;
+            }
+            else
+            {
+                ctrlText.Text += button.Text;
+                isOperatorPerforemed = false;
+                shouldAddToListAndText = false;
+            }
+
+            // TODO: Not add dot after symbol, but if it is 0. then add
+            // TODO: Fix bug with not adding 0 at all
+            if (button.Text == "0")
+            {
+                string text = ctrlOperation.Text;
+                string lastValueInText = text[text.Length - 1].ToString();
+
+                foreach (var item in lsSymbols)
+                {
+                    if (!text.EndsWith(item) && item == lastValueInText)
+                    {
+                        shouldAddToListAndText = true;
+                    }
+                }
+            }
+            else
+            {
+                shouldAddToListAndText = true;
+            }
+
+            // DONE not to add operation after operation
+            if (lsValues.Count > 0)
+            {
+                if (lsSymbols.Contains(button.Text) && lsSymbols.Contains(lsValues.Last()))
+                {
+                    shouldAddToListAndText = false;
+                }
+            }
+
+            if (shouldAddToListAndText)
+            {
+                ctrlOperation.Text += button.Text;
+                lsValues.Add(button.Text);
+            }
+
+
+        }
+
+        // TODO: check if C or CE bt should delete the ctrl operationText and/or just the ctrlText
+        private void CEButton_click(object sender, EventArgs e)
+        {
+            ctrlText.Text = "0";
+        }
+
+        private void CButton_click(object sender, EventArgs e)
+        {
+            ctrlText.Text = "0";
+        }
+
+        private void EqualButton_click(object sender, EventArgs e)
+        {
+            //TODO: 
+
+        }
+
+        // Good thinking, deploy
+        //public void method2()
+        //{
+        //    List<string> inputList = new List<string> { "5", "5", "*", "4", "-", "2" };
+        //    int index = 0;
+        //    int operand1 = 0;
+        //    int operand2 = 0;
+        //    string operation = "";
+        //    while (index < inputList.Count)
+        //    {
+        //        if (int.TryParse(inputList[index], out int num))
+        //        {
+        //            if (operand1 == 0)
+        //            {
+        //                operand1 = num;
+        //            }
+        //            else
+        //            {
+        //                operand2 = num;
+        //                switch (operation)
+        //                {
+        //                    case "+":
+        //                        operand1 += operand2;
+        //                        break;
+        //                    case "-":
+        //                        operand1 -= operand2;
+        //                        break;
+        //                    case "*":
+        //                        operand1 *= operand2;
+        //                        break;
+        //                    case "/":
+        //                        operand1 /= operand2;
+        //                        break;
+        //                }
+        //            }
+        //        }
+        //        else
+        //        {
+        //            operation = inputList[index];
+        //        }
+        //        index++;
+        //    }
+        //    ctrlText.Text = Convert.ToString(operation);
+        //}
+
+
+    }
+}
